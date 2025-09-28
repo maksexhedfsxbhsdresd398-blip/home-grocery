23ca99bc6281289f695db6e3500c765d925f92e0
maksexhedfsxbhsdresd398-blip <maksexhedfsxbhsdresd398@gmail.com>
2025-09-28 11:57:24 +0500

    Update ConfirmClearDialog.kt

--- app/src/main/java/com/example/grocerynative/ui/dialogs/ConfirmClearDialog.kt
+++ app/src/main/java/com/example/grocerynative/ui/dialogs/ConfirmClearDialog.kt
@@ -5,16 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 
-class ConfirmDeleteDialog(private val onOk:()->Unit): DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        AlertDialog.Builder(requireContext())
-            .setTitle("Confirm Deletion")
-            .setMessage("Delete this item permanently?")
-            .setPositiveButton("Delete") {_,_-> onOk() }
-            .setNegativeButton("Cancel", null)
-            .create()
-}
-
 class ConfirmClearDialog(private val onOk:()->Unit): DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
         AlertDialog.Builder(requireContext())
