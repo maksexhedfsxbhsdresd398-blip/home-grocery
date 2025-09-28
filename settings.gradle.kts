463241a59e961e04d0a178717ae718a3b8d38f6d
maksexhedfsxbhsdresd398-blip <maksexhedfsxbhsdresd398@gmail.com>
2025-09-28 11:43:08 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
+pluginManagement {
+    repositories {
+        google()            // <-- required for Android Gradle Plugin
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "GroceryNative"
 include(":app")
